5958648e2e3716c7fb7fa3aa7bdd53c9b420249e
JKasyan <j.kasyan86@gmail.com>
2016-03-17 23:56:24 +0200

    item17

---
+++ src/main/java/item17/Super.java
@@ -0,0 +1,37 @@
+package item17;
+
+import java.util.Date;
+
+/**
+ * Created by evgen on 17.03.16.
+ */
+public class Super {
+
+    protected Super() {
+        overrideMe();
+    }
+
+    public void overrideMe() {}
+}
+
+class Sub extends Super {
+
+    private final Date date;
+
+    public Sub(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public void overrideMe() {
+        System.out.println(date);
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) {
+        Sub sub = new Sub(new Date());
+        sub.overrideMe();
+    }
+}
